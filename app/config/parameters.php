4e97f7fcc618d869f12b12304881f85e3f4b81fa
Fabian Vogler <fabian@equivalence.ch>
2012-09-10 22:36:20 +0200

    Use parameters.php

---
+++ app/config/parameters.php
@@ -0,0 +1,17 @@
+<?php 
+
+$container->setParameter('database_driver', 'pdo_mysql');
+$container->setParameter('database_host', 'localhost');
+$container->setParameter('database_port', null);
+$container->setParameter('database_name', 'lightning');
+$container->setParameter('database_name_test', 'lightning_test');
+$container->setParameter('database_user', 'root');
+$container->setParameter('database_password', null);
+$container->setParameter('database_path', null);
+
+$container->setParameter('mailer_transport', 'smtp');
+$container->setParameter('mailer_host', 'localhost');
+$container->setParameter('mailer_user', null);
+$container->setParameter('mailer_password', null);
+$container->setParameter('locale', 'en');
+$container->setParameter('secret', '579f85a6a7cb52df9101107ba82714f509');
