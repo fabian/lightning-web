28f41198381005fe9054f8070ce3208fc7913f22
Fabian Vogler <fabian@equivalence.ch>
2012-09-10 15:21:10 +0200

    Added Login

--- src/Lightning/ApiBundle/Resources/public/js/lists.js
+++ src/Lightning/ApiBundle/Resources/public/js/lists.js
@@ -2,6 +2,35 @@ var Lightning = Lightning || {};
 
 $(function () {
 
+    var url = Lightning.URL_ACCOUNT;
+
+    var container = $('#container');
+    container.html('<p>To access your lists you need to send an access request to your mobile phone.</p>'); 
+
+    var message = $(document.createElement('p'));
+    var link = $(document.createElement('a')).attr('href', url).text('Send Request');
+    link.click(function (e) {
+
+        var i = 0;
+        setInterval(function () {
+            i = (i + 1) % 4;
+            var message = '<p>Sending access request';
+            for (var j = 0; j < i; j++) {
+                message += '.';
+            }
+            message += '</p>';
+            container.html(message);
+        }, 300);
+
+        return false;
+    });
+    message.append(link);
+    container.append(message);
+
+    $('#user').html('<a href="">Logout</a>');
+
+    return;
+
     $.ajax({
         url: Lightning.URL_LISTS,
         dataType: 'json',
