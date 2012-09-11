57cf70f45b6fef8d532ef6a3f79ae0c5b2042fef
Fabian Vogler <fabian@equivalence.ch>
2012-09-11 19:30:39 +0200

    Adjust test for new url in list

--- src/Lightning/ApiBundle/Tests/Controller/ListControllerTest.php
+++ src/Lightning/ApiBundle/Tests/Controller/ListControllerTest.php
@@ -50,6 +50,6 @@ class ListControllerTest extends ApiControllerTest
             'HTTP_ACCOUNT' => 'http://localhost/accounts/1?secret=123',
             'HTTP_ACCEPT' => 'application/json',
         ));
-        $this->assertEquals('{"lists":[{"id":1,"title":"Example"}]}', $client->getResponse()->getContent());
+        $this->assertEquals('{"lists":[{"id":1,"title":"Example","url":"http:\/\/localhost\/lists\/1"}]}', $client->getResponse()->getContent());
     }
 }
