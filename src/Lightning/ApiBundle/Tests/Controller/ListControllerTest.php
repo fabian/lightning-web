4cde31605eed8adc1fd20d0e639e3c49ae3fc2bb
Fabian Vogler <fabian@equivalence.ch>
2012-09-19 22:40:14 +0200

    Added more tests for lists controller

--- src/Lightning/ApiBundle/Tests/Controller/ListControllerTest.php
+++ src/Lightning/ApiBundle/Tests/Controller/ListControllerTest.php
@@ -24,5 +24,32 @@ class ListControllerTest extends ApiControllerTest
         $this->assertEquals('{"id":1,"title":"Groceries","url":"http:\/\/localhost\/lists\/1"}', $client->getResponse()->getContent());
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
-    
+
+    public function testShowWrongId()
+    {
+        $client = static::createClient(array('debug' => false));
+
+        $crawler = $client->request('GET', '/lists/999', array(), array(), array(
+            'HTTP_ACCOUNT' => 'http://localhost/accounts/1?secret=123',
+            'HTTP_ACCEPT' => 'application/json',
+        ));
+
+        $this->assertEquals('{"error":{"code":404,"message":"No list found for id 999."}}', trim($client->getResponse()->getContent()));
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testShowWrongAccount()
+    {
+        $client = static::createClient(array('debug' => false));
+
+        $this->createAccount();
+
+        $crawler = $client->request('GET', '/lists/1', array(), array(), array(
+            'HTTP_ACCOUNT' => 'http://localhost/accounts/2?secret=123',
+            'HTTP_ACCEPT' => 'application/json',
+        ));
+
+        $this->assertEquals('{"error":{"code":403,"message":"Authenticated account 2 has no access to list."}}', trim($client->getResponse()->getContent()));
+        $this->assertEquals(403, $client->getResponse()->getStatusCode());
+    }
 }
