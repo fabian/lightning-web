e52eb7083ffb9c24734742f53c8926ddf783e58b
Fabian Vogler <fabian@equivalence.ch>
2012-10-06 20:02:02 +0200

    Added missing item tests

--- src/Lightning/ApiBundle/Tests/Controller/ItemControllerTest.php
+++ src/Lightning/ApiBundle/Tests/Controller/ItemControllerTest.php
@@ -159,6 +159,31 @@ class ItemControllerTest extends ApiControllerTest
         $this->assertEquals(404, $response->getStatusCode());
     }
 
+    public function testShowWrongAccount()
+    {
+        $this->createItem($this->list);
+        $this->createAccount();
+
+        $this->client->request(
+            'GET',
+            '/items/1',
+            array(),
+            array(),
+            array(
+                'HTTP_ACCOUNT' => 'http://localhost/accounts/2?secret=123',
+                'HTTP_ACCEPT' => 'application/json',
+            )
+        );
+
+        $response = $this->client->getResponse();
+        $this->assertEquals(
+            '{"error":{"code":403,"message":"Authenticated account 2 has no access to list."}}',
+            trim($response->getContent())
+        );
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
     public function testUpdate()
     {
         $this->createItem($this->list);
@@ -221,6 +246,69 @@ class ItemControllerTest extends ApiControllerTest
         $this->assertEquals(409, $response->getStatusCode());
     }
 
+    public function testUpdateWrongId()
+    {
+        $this->createItem($this->list);
+
+        $this->client->request(
+            'PUT',
+            '/items/999',
+            array('value' => 'Coffee', 'done' => '1'),
+            array(),
+            array(
+                'HTTP_ACCOUNT' => 'http://localhost/accounts/1?secret=123',
+                'HTTP_ACCEPT' => 'application/json',
+            )
+        );
+
+        $item = $this->em
+            ->getRepository('LightningApiBundle:Item')
+            ->find(1);
+
+        $this->assertEquals('Milk', $item->getValue());
+        $this->assertFalse($item->getDone());
+
+        $response = $this->client->getResponse();
+        $this->assertEquals(
+            '{"error":{"code":404,"message":"No item found for id 999."}}',
+            trim($response->getContent())
+        );
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testUpdateWrongAccount()
+    {
+        $this->createItem($this->list);
+        $this->createAccount();
+
+        $this->client->request(
+            'PUT',
+            '/items/1',
+            array('value' => 'Coffee', 'done' => '1'),
+            array(),
+            array(
+                'HTTP_ACCOUNT' => 'http://localhost/accounts/2?secret=123',
+                'HTTP_ACCEPT' => 'application/json',
+            )
+        );
+
+        $item = $this->em
+            ->getRepository('LightningApiBundle:Item')
+            ->find(1);
+
+        $this->assertEquals('Milk', $item->getValue());
+        $this->assertFalse($item->getDone());
+
+        $response = $this->client->getResponse();
+        $this->assertEquals(
+            '{"error":{"code":403,"message":"Authenticated account 2 has no access to list."}}',
+            trim($response->getContent())
+        );
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
     public function testDelete()
     {
         $this->createItem($this->list);
@@ -246,5 +334,66 @@ class ItemControllerTest extends ApiControllerTest
         $this->assertEquals('', $response->getContent());
         $this->assertEquals('application/json', $response->headers->get('Content-Type'));
         $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testDeleteWrongId()
+    {
+        $this->createItem($this->list);
+
+        $this->client->request(
+            'DELETE',
+            '/items/999',
+            array(),
+            array(),
+            array(
+                'HTTP_ACCOUNT' => 'http://localhost/accounts/1?secret=123',
+                'HTTP_ACCEPT' => 'application/json',
+            )
+        );
+
+        $item = $this->em
+            ->getRepository('LightningApiBundle:Item')
+            ->find(1);
+
+        $this->assertFalse($item->getDeleted());
+
+        $response = $this->client->getResponse();
+        $this->assertEquals(
+            '{"error":{"code":404,"message":"No item found for id 999."}}',
+            trim($response->getContent())
+        );
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testDeleteWrongAccount()
+    {
+        $this->createItem($this->list);
+        $this->createAccount();
+
+        $this->client->request(
+            'DELETE',
+            '/items/1',
+            array(),
+            array(),
+            array(
+                'HTTP_ACCOUNT' => 'http://localhost/accounts/2?secret=123',
+                'HTTP_ACCEPT' => 'application/json',
+            )
+        );
+
+        $item = $this->em
+            ->getRepository('LightningApiBundle:Item')
+            ->find(1);
+
+        $this->assertFalse($item->getDeleted());
+
+        $response = $this->client->getResponse();
+        $this->assertEquals(
+            '{"error":{"code":403,"message":"Authenticated account 2 has no access to list."}}',
+            trim($response->getContent())
+        );
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        $this->assertEquals(403, $response->getStatusCode());
     }
 }
