342e813004118e58a5df5cce0ba594be5453aafa
Fabian Vogler <fabian@equivalence.ch>
2012-10-08 21:34:13 +0200

    Removing PHPUnit autoload

--- app/autoload.php
+++ app/autoload.php
@@ -4,8 +4,6 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-require_once 'PHPUnit/Autoload.php';
-
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
