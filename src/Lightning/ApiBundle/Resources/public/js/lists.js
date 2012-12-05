5b67a29ba8c8368fe303d2158d219e466d2209cd
Fabian Vogler <fabian@equivalence.ch>
2012-12-05 08:20:52 +0100

    Fixed coding conventions

--- src/Lightning/ApiBundle/Resources/public/js/lists.js
+++ src/Lightning/ApiBundle/Resources/public/js/lists.js
@@ -83,10 +83,14 @@ Lightning.App.prototype.writeCookie = function (name, value, days) {
 Lightning.App.prototype.readCookie = function (name) {
     var nameEQ = name + '=';
     var ca = document.cookie.split(';');
-    for (var i=0;i < ca.length;i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        while (c.charAt(0)==' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
     }
     return null;
 };
